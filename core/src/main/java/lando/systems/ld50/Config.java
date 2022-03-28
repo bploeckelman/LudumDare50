dd4984420c5a2766fdac29ebc52a8ef0a56350e0
Doug Graham <dsgraham@gmail.com>
2022-03-28 17:18:41 -0500

    Jenkinsfile, sprites task and basic Configs

---
+++ core/src/main/java/lando/systems/ld50/Config.java
@@ -0,0 +1,10 @@
+package lando.systems.ld50;
+
+public class Config {
+    public static final String title = "Ludum Dare 50";
+    public static final int window_width = 1280;
+    public static final int window_height = 720;
+    public static final int viewport_width = 1280;
+    public static final int viewport_height = 720;
+    public static final boolean show_launch_screen = false;
+}
--- lwjgl3/src/main/java/lando/systems/ld50/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/lando/systems/ld50/lwjgl3/Lwjgl3Launcher.java
@@ -2,6 +2,7 @@ package lando.systems.ld50.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import lando.systems.ld50.Config;
 import lando.systems.ld50.Main;
 
 /** Launches the desktop (LWJGL3) application. */
@@ -16,14 +17,14 @@ public class Lwjgl3Launcher {
 
 	private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
 		Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
-		configuration.setTitle("LudumDare50");
+		configuration.setTitle(Config.title);
 		configuration.useVsync(true);
 		//// Limits FPS to the refresh rate of the currently active monitor.
 		configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
 		//// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
 		//// useful for testing performance, but can also be very stressful to some hardware.
 		//// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
-		configuration.setWindowedMode(640, 480);
+		configuration.setWindowedMode(Config.window_width, Config.window_height);
 		configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
 		return configuration;
 	}
