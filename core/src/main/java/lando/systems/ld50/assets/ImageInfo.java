d7204b030aed70b1c80c2930ae215e1886aad14c
Brian <brossman@kiio.com>
2022-04-03 23:28:20 -0500

    scream in enum

--- core/src/main/java/lando/systems/ld50/assets/ImageInfo.java
+++ core/src/main/java/lando/systems/ld50/assets/ImageInfo.java
@@ -1,27 +1,27 @@
 package lando.systems.ld50.assets;
 
+import static lando.systems.ld50.audio.AudioManager.*;
+
 public enum ImageInfo {
-    BabeA ("characters/babe-a", "characters/babe-a-wave", 0.1f),
-    BabeB ("characters/babe-b", "characters/babe-b-wave", 0.1f),
-    Dude ("characters/dude-a", null, 0.1f),
-    Deer ("characters/deer-a", null, 0.1f),
-    Plow ("characters/plow-a", null, 0.1f, 7f, 4f, false);
+    BabeA ("characters/babe-a", "characters/babe-a-wave", 0.1f, Sounds.none, 0.5f, 0.5f, true),
+    BabeB ("characters/babe-b", "characters/babe-b-wave", 0.1f, Sounds.none, 0.5f, 0.5f, true),
+    Dude ("characters/dude-a", null, 0.1f, Sounds.none, 0.5f, 0.5f, true),
+    Deer ("characters/deer-a", null, 0.1f, Sounds.none, 0.5f, 0.5f, true),
+    Plow ("characters/plow-a", null, 0.1f, Sounds.none,7f, 4f, false);
 
     public String region;
     public String waveRegion;
+    public Sounds scream;
     public float width;
     public float height;
     public float frameDuration;
     public boolean right;
 
-    ImageInfo(String region, String waveRegion, float duration) {
-        this(region, waveRegion, duration, 0.5f, 0.5f, true);
-    }
-
-    ImageInfo(String region, String waveRegion, float duration, float width, float height, boolean right) {
+    ImageInfo(String region, String waveRegion, float duration, Sounds scream, float width, float height, boolean right) {
         this.region = region;
         this.waveRegion = waveRegion;
         this.frameDuration = duration;
+        this.scream = scream;
         this.width = width;
         this.height = height;
         this.right = right;
