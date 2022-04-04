2cec45a6088b9c78ed2100b9b206560ba4af2976
Doug Graham <dsgraham@wisc.edu>
2022-04-04 14:30:24 -0500

    stub epilogue screen

---
+++ core/src/main/java/lando/systems/ld50/screens/EpilogueScreen.java
@@ -0,0 +1,14 @@
+package lando.systems.ld50.screens;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class EpilogueScreen extends BaseScreen {
+
+    // TODO: Show burning broken lodge building
+    // and some exposition about what happened
+
+    @Override
+    public void render(SpriteBatch batch) {
+
+    }
+}
