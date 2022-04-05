c0a21b9309b28844c08ac6f077ca1da293f54e71
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2022-04-04 19:39:30 -0500

    Disable resizing in desktop

--- lwjgl3/src/main/java/lando/systems/ld50/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/lando/systems/ld50/lwjgl3/Lwjgl3Launcher.java
@@ -26,6 +26,7 @@ public class Lwjgl3Launcher {
 		//// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
 		configuration.setWindowedMode(Config.window_width, Config.window_height);
 		configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+		configuration.setResizable(false);
 		return configuration;
 	}
 }
